64815df31d279e8f2b39849aa3d86921d428ac07
Elias Gottsbacher <eli.gottsbacher@gmail.com>
2024-05-31 17:50:18 +0200

    fix docs

--- src/lib.rs
+++ src/lib.rs
@@ -5,24 +5,40 @@
 //! # Example
 //!
 //! ```rust
-//! # use egui_macros::load_layout;
-//! let layout_code = load_layout!(
-//!     <Strip direction="west">
-//!         <Panel size="relative" value="0.3">
-//!             color_background(ui, egui::Color32::from_rgb(0, 0, 255));
-//!         </Panel>
-//!         <Panel size="remainder">
-//!             <Strip direction="north">
-//!                 <Panel size="relative" value="0.3">
-//!                     color_background(ui, egui::Color32::from_rgb(0, 255, 255));
-//!                 </Panel>
-//!                 <Panel size="remainder">
-//!                     color_background(ui, egui::Color32::from_rgb(255, 0, 255));
-//!                 </Panel>
-//!             </Strip>
-//!         </Panel>
-//!     </Strip>
-//! );
+//! use eframe::egui;
+//! use egui::{Rounding, Ui};
+//! use egui_xml::load_layout;
+//!
+//! struct MyApp;
+//!
+//! fn color_background(ui: &mut Ui, color: egui::Color32) {
+//!     ui.painter()
+//!         .rect_filled(ui.available_rect_before_wrap(), Rounding::same(5.0), color);
+//! }
+//!
+//! impl eframe::App for MyApp {
+//!     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+//!         egui::CentralPanel::default().show(ctx, |ui| {
+//!             load_layout!(
+//!                 <Strip direction="west">
+//!                     <Panel size="relative" value="0.3">
+//!                         color_background(ui, egui::Color32::from_rgb(0, 0, 255));
+//!                     </Panel>
+//!                     <Panel size="remainder">
+//!                         <Strip direction="north">
+//!                             <Panel size="relative" value="0.3">
+//!                                 color_background(ui, egui::Color32::from_rgb(0, 255, 255));
+//!                             </Panel>
+//!                             <Panel size="remainder">
+//!                                 color_background(ui, egui::Color32::from_rgb(255, 0, 255));
+//!                             </Panel>
+//!                         </Strip>
+//!                     </Panel>
+//!                 </Strip>
+//!             );
+//!         });
+//!     }
+//! }
 //! ```
 
 pub use egui_xml_macros::load_layout;
